bee21bc29f9374273e091ff82035da6d41135a26
Unknown <f.noel@rubika-edu.com>
2020-04-27 18:30:27 +0200

    Starting following plateform

--- Assets/Scripts/LD_Behaviours/Following_Plateform.cs
+++ Assets/Scripts/LD_Behaviours/Following_Plateform.cs
@@ -1,49 +1,72 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+//using System.Numerics;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Following_Plateform : MonoBehaviour
 {
     [Header("Plateform Movement")]
-    [SerializeField] private Transform plateformPos;
+    public Transform plateformPos;
+    public Transform cameraRef;
+    public BoxCollider plateformCollid;
 
-    //public cameraRef
-    [SerializeField] private Vector2 nextPos;
-    private float plateformSpeed;
-    private float yMax;
-    private float yMin;
+    private float diff;
+    private float plateformSpeed = 10f;
+    private float moveRange = 0.3f;
+
+    public LayerMask layerMask;
 
     [SerializeField] private bool inCameraRange;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        ComparePlaterformAndCameraPositions();
     }
 
     void ComparePlaterformAndCameraPositions()
     {
-        //if()
-        //{
-        //    nextPos = 
-        //}
+        //Get magnitude
+        diff = cameraRef.localPosition.y - plateformPos.localPosition.y;
+
+        if (Mathf.Abs(diff) > moveRange)
+        {
+            if (!IsThereAnObstacleInThisDirection(Vector3.up * Math.Sign(diff)))
+            {
+                MoveToThisDirection();
+            }
+        }
     }
 
-    void MoveToNextPos()
+    void MoveToThisDirection()
     {
-        plateformPos.position = new Vector3(plateformPos.position.x, nextPos.y, plateformPos.position.z);
+        float newYPos = plateformPos.position.y + Math.Sign(diff) * plateformSpeed * Time.deltaTime;
+
+        plateformPos.position = new Vector3(plateformPos.position.x, newYPos, plateformPos.position.z);
     }
 
-    bool IsThereObstacles()
+    bool IsThereAnObstacleInThisDirection(Vector3 direction)
     {
-        //RaycastHit2D upResult = Physics2D.BoxCast(plateformPos.position, )
+        RaycastHit result;
+        float maxDistance = 0.2f;
 
-        return false;
+        Physics.BoxCast(plateformPos.position,
+            plateformPos.lossyScale * 0.5f,
+            direction,
+            out result,
+            Quaternion.identity,
+            maxDistance,
+            layerMask);
+
+        if (result.collider != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
