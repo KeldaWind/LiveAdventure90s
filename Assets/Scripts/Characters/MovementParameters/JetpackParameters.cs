2abe556853f86e524691bfcfd60fe86bbb9b864a
Sebastien <sebastien.butor@orange.fr>
2020-04-28 14:28:33 +0200

    Jetpack parameters

---
+++ Assets/Scripts/Characters/MovementParameters/JetpackParameters.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "LiveAd/Characters/Jetpack Parameters", fileName = "JetpackParameters")]
+public class JetpackParameters : ScriptableObject
+{
+    [SerializeField] float jetpackMaxUpSpeed = 10f;
+    public float GetJetpackMaxUpSpeed => jetpackMaxUpSpeed;
+
+
+    [SerializeField] float jetpackMaxDownSpeed = -10f;
+    public float GetJetpackMaxDownSpeed => jetpackMaxDownSpeed;
+
+
+    [SerializeField] float jetpackUpAcceleration = 20f;
+    public float GetJetpackUpAcceleration => jetpackUpAcceleration;
+
+
+    [SerializeField] float jetpackGravityWhenGoingUp = -10f;
+    public float GetJetpackGravityWhenGoingUp => jetpackGravityWhenGoingUp;
+
+
+    [SerializeField] float jetpackGravityWhenGoingDown = -10f;
+    public float GetJetpackGravityWhenGoingDown => jetpackGravityWhenGoingDown;
+}
---
+++ Assets/Scripts/Projectile/ProjectileEnemyBarrel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileEnemyBarrel : ProjectileBase
+{
+    public override void Update()
+    {
+        base.Update();
+    }
+
+    [Header("Physics")]
+    [SerializeField] BoxRaycaster barrelRaycaster = default;
+    [SerializeField] float gravity = 30f;
+    [SerializeField] float maxVerticalDownSpeed = 30f;
+
+    public override void HandleCollision(RaycastHit hit)
+    {
+
+    }
+
+    public override void UpdateTrajectory()
+    {
+
+    }
+}
