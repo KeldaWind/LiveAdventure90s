8fda587a7f622669624b2faaba9620cb13868cec
Unknown <f.noel@rubika-edu.com>
2020-04-28 22:36:28 +0200

    BallHolder

--- Assets/Scripts/LD_Behaviours/Laser_Behaviour.cs
+++ Assets/Scripts/LD_Behaviours/Laser_Behaviour.cs
@@ -10,6 +10,10 @@ public class Laser_Behaviour : MonoBehaviour
     public Vector3 startPos;
     public GameObject targetPos;
 
+    private Transform laserCollider;
+
+    private bool isActive = true;
+
     private Vector3 impactPos;
 
 
@@ -27,23 +31,40 @@ public class Laser_Behaviour : MonoBehaviour
 
     public void SetLaserState(bool value)
     {
+        isActive = value;
         lineRenderer.enabled = value;
+    }
+    
+    void SetLaserCollider(float magnitude, Vector3 direction)
+    {
+        Quaternion newRotation = Quaternion.LookRotation(direction, Vector3.up);
+        laserCollider.localScale = new Vector3(magnitude, 0.5f, 1f);
+        laserCollider.rotation = newRotation;
+        laserCollider.localPosition = direction * (magnitude * 0.5f);
     }
 
     void FindNextTarget()
     {
-        Vector3 direction = (targetPos.transform.position - lineRenderer.GetPosition(0)).normalized;
-        RaycastHit hit;
-
-        Physics.Raycast(lineRenderer.GetPosition(0), direction, out hit, Mathf.Infinity);
-
-        if (hit.collider != null)
-            impactPos = hit.point;
-        lineRenderer.SetPosition(1, impactPos);
-
-        if (IsCollidingWithPlayer(hit))
+        if(isActive)
         {
-            //Receive Damage ??
+            float magnitude = (targetPos.transform.position - lineRenderer.GetPosition(0)).magnitude;
+            Vector3 direction = (targetPos.transform.position - lineRenderer.GetPosition(0)).normalized;
+            RaycastHit hit;
+
+            Physics.Raycast(lineRenderer.GetPosition(0), direction, out hit, Mathf.Infinity);
+
+            if (hit.collider != null)
+                impactPos = hit.point;
+            lineRenderer.SetPosition(1, impactPos);
+
+
+
+            //SetLaserCollider(magnitude, direction);
+
+            if (IsCollidingWithPlayer(hit))
+            {
+                //Receive Damage ??
+            }
         }
     }
 
