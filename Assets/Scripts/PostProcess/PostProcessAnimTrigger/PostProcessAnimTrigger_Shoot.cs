b83e5c96ef2d2b5059aba4ab985348b71b29d85c
VINCENT TRINEL <v.trinel@rubika-edu.com>
2020-04-30 12:48:30 +0200

    J'ai tué Hugo Dukro le bogoss

--- Assets/Scripts/PostProcess/PostProcessAnimTrigger/PostProcessAnimTrigger_Shoot.cs
+++ Assets/Scripts/PostProcess/PostProcessAnimTrigger/PostProcessAnimTrigger_Shoot.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class PostProcessAnimTrigger_Shoot : PostProcessAnimTrigger
 {
+    [SerializeField] ThirdPersonController thirdPersonController;
+
     private void OnEnable()
     {
+        thirdPersonController.OnPlayerShotProjectile += PlayPostProcessAnim;
         //subscribe
     }
 
     private void OnDisable()
     {
+
+        thirdPersonController.OnPlayerShotProjectile -= PlayPostProcessAnim;
         //unsubscribe
     }
 }
